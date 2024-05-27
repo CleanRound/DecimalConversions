708ca92e47e1bdc3e3216c2a3ec21d85306fa0d4
Олег Луцик <oleglutsik.dev@gmail.com>
2024-05-27 16:31:33 +0300

    Добавьте файлы проекта.

---
+++ DecimalConversions/Program.cs
@@ -0,0 +1,46 @@
+﻿public struct DecimalNumber
+{
+    public int Number { get; }
+
+    public DecimalNumber(int number)
+    {
+        Number = number;
+    }
+
+    public string ToBinary()
+    {
+        return Convert.ToString(Number, 2);
+    }
+
+    public string ToOctal()
+    {
+        return Convert.ToString(Number, 8);
+    }
+
+    public string ToHexadecimal()
+    {
+        return Convert.ToString(Number, 16).ToUpper();
+    }
+
+    public override string ToString()
+    {
+        return Number.ToString();
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        DecimalNumber decimalNumber = new DecimalNumber(255);
+
+        string binary = decimalNumber.ToBinary();
+        Console.WriteLine($"Decimal {decimalNumber} in binary: {binary}");
+
+        string octal = decimalNumber.ToOctal();
+        Console.WriteLine($"Decimal {decimalNumber} in octal: {octal}");
+
+        string hexadecimal = decimalNumber.ToHexadecimal();
+        Console.WriteLine($"Decimal {decimalNumber} in hexadecimal: {hexadecimal}");
+    }
+}
